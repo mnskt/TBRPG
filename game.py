5df0bd73572aeeab8e3a82e36a6c709299589714
mnskt <mantas.milieska@gmail.com>
2020-09-21 21:56:09 +0300

    Added game setup function

--- game.py
+++ game.py
@@ -7,14 +7,51 @@ import random
 screen_width = 100
 
 # Player Setup
-def Player:
+class Player():
 	def __init__(self):
 		self.name = ''
 		self.age = 0
 		self.gender = ''
-		self.astro = ''
 		self.race = ''
 		self.klass = ''
 
-MyPlayer = Player()
+thePlayer = Player()
 
+def game_setup():
+	os.system('clear&cls')
+	# Get Name
+	obtain_name = "Hello, what's your name?\n"
+	for character in obtain_name:
+		sys.stdout.write(character)
+		sys.stdout.flush()
+		time.sleep(0.04)
+	player_name = input(">>> ")
+	thePlayer.name = player_name
+	# Get Age
+	obtain_age = "And what is your age?\n"
+	for character in obtain_age:
+		sys.stdout.write(character)
+		sys.stdout.flush()
+		time.sleep(0.04)
+	player_age = input(">>> ")
+	thePlayer.age = player_age
+	# Get gender
+	obtain_gender = "Are you male or female?\n"
+	for character in obtain_gender:
+		sys.stdout.write(character)
+		sys.stdout.flush()
+		time.sleep(0.04)
+	player_gender = input(">>> ")
+	while player_gender.lower() not in ['male', 'female']:
+		if player_gender.lower() == 'Male':
+			thePlayer.gender = player_gender
+		elif player_gender.lower() == "Female":
+			thePlayer.gender = player_gender
+		else:
+			print("Please select either male or female genders.")
+
+	print("Your name is " + thePlayer.name + ".")
+	print("Your are " + thePlayer.age + " years old.")
+	print("Your are " + thePlayer.gender + ".")
+
+game_setup()
