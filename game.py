825fd0ee3a74af34754ec9da4369f9a4e0f2093f
mnskt <mantas.milieska@gmail.com>
2020-09-21 22:32:28 +0300

    Added stuff

--- game.py
+++ game.py
@@ -17,41 +17,69 @@ class Player():
 
 thePlayer = Player()
 
+def text_write(value):
+	for character in value:
+		sys.stdout.write(character)
+		sys.stdout.flush()
+		time.sleep(0.04)
+
 def game_setup():
 	os.system('clear&cls')
+
 	# Get Name
 	obtain_name = "Hello, what's your name?\n"
-	for character in obtain_name:
-		sys.stdout.write(character)
-		sys.stdout.flush()
-		time.sleep(0.04)
+	text_write(obtain_name)
 	player_name = input(">>> ")
 	thePlayer.name = player_name
+
 	# Get Age
 	obtain_age = "And what is your age?\n"
-	for character in obtain_age:
-		sys.stdout.write(character)
-		sys.stdout.flush()
-		time.sleep(0.04)
+	text_write(obtain_age)
 	player_age = input(">>> ")
 	thePlayer.age = player_age
+
 	# Get gender
 	obtain_gender = "Are you male or female?\n"
-	for character in obtain_gender:
-		sys.stdout.write(character)
-		sys.stdout.flush()
-		time.sleep(0.04)
+	text_write(obtain_gender)
 	player_gender = input(">>> ")
-	while player_gender.lower() not in ['male', 'female']:
-		if player_gender.lower() == 'Male':
-			thePlayer.gender = player_gender
-		elif player_gender.lower() == "Female":
-			thePlayer.gender = player_gender
-		else:
-			print("Please select either male or female genders.")
-
+	acceptable_genders = ['male', 'female']
+	while player_gender.lower() not in acceptable_genders:
+		print("Unacceptable gender, try again")
+		player_gender = input(">>> ")
+	thePlayer.gender = player_gender.lower()
 	print("Your name is " + thePlayer.name + ".")
 	print("Your are " + thePlayer.age + " years old.")
 	print("Your are " + thePlayer.gender + ".")
 
-game_setup()
+def title_screen():
+	os.system("clear&cls")
+	print("########################")
+	print("#  -Welcome to TBRPG-  #")
+	print("########################")
+	print("###   - New Game -   ###")
+	print("###     - Help -     ###")
+	print("###     - Exit -     ###")
+	print("########################")
+	title_screen_nav()
+
+def title_screen_nav():
+	title_screen_option = input(">>> ")
+	acceptable_choices = ['new game', 'help', 'exit']
+	while title_screen_option.lower() not in acceptable_choices:
+		print("Unavailable option, try again")
+		title_screen_option = input(">>> ")
+		if title_screen_option.lower() == ("new game"):
+			game_setup()
+		elif title_screen_option.lower() == ("help"):
+			help_menu()
+		elif title_screen_option.lower() == ("exit"):
+			sys.exit()
+
+def help_menu():
+	print("########################")
+	print("#  -Welcome to TBRPG-  #")
+	print("########################")
+	print("Help description.")
+	title_screen_nav()
+
+title_screen()
