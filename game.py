ed0aa644b9ac01e0eafb2aa76eeb2f57590b5a9a
mnskt <mantas.milieska@gmail.com>
2020-09-21 21:24:47 +0300

    Added classes and races

--- game.py
+++ game.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+import cmd
+import sys
+import os
+import time
+import random
+
+screen_width = 100
+
+# Player Setup
+def Player:
+	def __init__(self):
+		self.name = ''
+		self.age = 0
+		self.gender = ''
+		self.astro = ''
+		self.race = ''
+		self.klass = ''
+
+MyPlayer = Player()
+
