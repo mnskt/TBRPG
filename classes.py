8c1f645e14a51b72fd5e534ae2236a7b0027a181
mnskt <mantas.milieska@gmail.com>
2020-09-21 21:23:58 +0300

    Added classes and races

---
+++ classes.py
@@ -0,0 +1,22 @@
+classes = {
+	'warrior': {
+		HP: 120,
+		MP: 0,
+		DESCRIPTION: 'description',
+		WEAPON: '2-handed rune sword'
+	},
+
+	'mage': {
+		HP: 50,
+		MP: 100,
+		DESCRIPTION: 'description',
+		WEAPON: 'Magic knife and thaumonomicon'
+	},
+
+	'cleric': {
+		HP: 60,
+		MP: 60,
+		DESCRIPTION: 'description',
+		WEAPON: 'Mace and a small shield'
+	}
+}
---
+++ races.py
@@ -0,0 +1,22 @@
+races = {
+	'human': {
+		HEIGHT: 190,
+		WEIGHT: 95,
+		RACE_HP: 40,
+		RACE_MP: 20
+	},
+
+	'elf': {
+		HEIGHT: 210,
+		WEIGHT: 85,
+		RACE_HP: 20,
+		RACE_MP: 40
+	},
+
+	'dwarf': {
+		HEIGHT: 140,
+		WEIGHT: 70,
+		RACE_HP: 30,
+		RACE_MP: 30
+	}
+}
